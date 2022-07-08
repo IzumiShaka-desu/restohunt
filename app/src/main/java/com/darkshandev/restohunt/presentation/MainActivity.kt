73c63436bf25db50365f5cf22448147c1668c45d
IzumiShaka-desu <shakaaji29@gmail.com>
2022-07-08 09:50:56 +0700

    refactor project structure

--- app/src/main/java/com/darkshandev/restohunt/presentation/MainActivity.kt
+++ app/src/main/java/com/darkshandev/restohunt/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.darkshandev.restohunt
+package com.darkshandev.restohunt.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.darkshandev.restohunt.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
