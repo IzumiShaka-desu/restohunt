9361c43948f5be8b42f75cdb4a6011036a7fd7cc
IzumiShaka-desu <shakaaji29@gmail.com>
2022-07-24 18:00:44 +0700

    create test case for validate favs item

--- favorites/src/test/java/com/darkshandev/favorites/FavoriteViewmodelTest.kt
+++ favorites/src/test/java/com/darkshandev/favorites/FavoriteViewmodelTest.kt
@@ -1,5 +1,53 @@
 package com.darkshandev.favorites
 
-import org.junit.jupiter.api.Assertions.*
+import app.cash.turbine.test
+import com.darkshandev.restohunt.core.domain.models.Restaurant
+import com.darkshandev.restohunt.core.domain.usecase.RestaurantUseCase
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
-internal class FavoriteViewmodelTest
+@RunWith(MockitoJUnitRunner::class)
+ class FavoriteViewmodelTest{
+@Mock
+private lateinit var useCase: RestaurantUseCase
+private lateinit var viewmodel: FavoriteViewmodel
+ @OptIn(ExperimentalCoroutinesApi::class)
+ @Before
+ fun setUp() {
+  Dispatchers.setMain(UnconfinedTestDispatcher())
+  viewmodel = FavoriteViewmodel(useCase)
+ }
+
+ @OptIn(ExperimentalCoroutinesApi::class)
+ @After
+ fun down() {
+  Dispatchers.resetMain()
+ }
+ @OptIn(ExperimentalCoroutinesApi::class)
+ @Test
+ fun `verify favorites items`() {
+  runTest {
+   val dummyFavs = listOf<Restaurant>()
+   Mockito.`when`(useCase.getFavRestaurants())
+    .thenReturn(flowOf(dummyFavs))
+   assertEquals(dummyFavs.size,viewmodel.favRestaurant.value.size)
+
+  }
+ }
+}
